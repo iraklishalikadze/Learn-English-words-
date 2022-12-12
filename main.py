3b28d0ce2f9500b15d405f5b8397f8dd871482c9
GREENCH\Greench Pc <irakli.shalikadze@gmail.com>
2022-12-13 00:19:26 +0400

    change

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ text_list = ["longest-serving,", "home"]
 engl_dic = [{"English":engli_word, "Georgia":word.translate(engli_word, dest="ka").text} for engli_word in text_list]
 print(engl_dic)
 
-print(1)
-print(2)
-print(3)
 
 
