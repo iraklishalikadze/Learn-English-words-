85e4e27d7cc7184aa2e099ac2a7fe8f019d73c8e
GREENCH\Greench Pc <irakli.shalikadze@gmail.com>
2022-12-13 00:16:21 +0400

    change

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ word = Translator()
 text_list = ["longest-serving,", "home"]
 engl_dic = [{"English":engli_word, "Georgia":word.translate(engli_word, dest="ka").text} for engli_word in text_list]
 print(engl_dic)
-<<<<<<< HEAD
+
 print(1)
 print(2)
 print(3)
 
-=======
-print(5)
->>>>>>> main
+
