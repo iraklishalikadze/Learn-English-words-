7378397d1cee08a6ef05dad744d8c5dc08ce9f0f
GREENCH\Greench Pc <irakli.shalikadze@gmail.com>
2023-01-06 22:31:08 +0400

    change readme file.

--- main.py
+++
@@ -1,6 +0,0 @@
-from googletrans import Translator
-import pandas
-word = Translator()
-text_list = ["longest-serving,", "home"]
-engl_dic = [{"English":engli_word, "Georgia":word.translate(engli_word, dest="ka").text} for engli_word in text_list]
-print(engl_dic)
